c7e127770758a6fdd56d9a1220a66f14cf40d2f1
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-28 23:57:56 +0300

    refactoring

--- src/main/java/setup/Driver.java
+++ src/main/java/setup/Driver.java
@@ -26,6 +26,7 @@ public class Driver extends TestProperties {
     protected String SUT; // site under testing
     protected String TEST_PLATFORM;
     protected String DRIVER;
+    protected String DEVICE_NAME;
 
     // Constructor for driver
     protected Driver() throws IOException {
@@ -46,16 +47,18 @@ public class Driver extends TestProperties {
             SUT = t_sut == null ? null : "http://" + t_sut;
             TEST_PLATFORM = getProp(WEB_TEST_PROPERTIES, "platform");
             DRIVER = getProp(WEB_TEST_PROPERTIES, "driver");
+            DEVICE_NAME = getProp(WEB_TEST_PROPERTIES, "deviceName");
         } else {
             AUT = getProp(NATIVE_TEST_PROPERTIES, "aut");
             TEST_PLATFORM = getProp(NATIVE_TEST_PROPERTIES, "platform");
             DRIVER = getProp(NATIVE_TEST_PROPERTIES, "driver");
+            DEVICE_NAME = getProp(NATIVE_TEST_PROPERTIES, "deviceName");
         }
 
         // Setup test platform: Android or iOS. Browser also depends on a platform.
         switch (TEST_PLATFORM) {
             case "Android":
-                capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554"); // default Android emulator
+                capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, DEVICE_NAME); // default Android emulator
                 browserName = "Chrome";
                 break;
             case "iOS":
