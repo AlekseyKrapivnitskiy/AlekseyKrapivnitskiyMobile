44d76c0896fb4b43c8d71e1cd958669ad607955f
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-28 18:01:56 +0300

    added assertions for native test

--- src/test/java/scenarios/nativetests/SimpleNativeTests.java
+++ src/test/java/scenarios/nativetests/SimpleNativeTests.java
@@ -6,6 +6,8 @@ import scenarios.Hooks;
 
 import java.io.IOException;
 
+import static org.testng.Assert.assertEquals;
+
 @Test(groups = "native")
 public class SimpleNativeTests extends Hooks {
     protected SimpleNativeTests() throws IOException {
@@ -14,10 +16,16 @@ public class SimpleNativeTests extends Hooks {
 
     @Test(description = "Just click on button 'Add contact'")
     public void simplestTest() throws Exception {
+        //1.Assert that title of start page
+        assertEquals(driver().findElement(By.id("android:id/title")).getText(), "Contact Manager");
+
+        //2.Click on 'Add Contact' button
         String app_package_name = "com.example.android.contactmanager:id/";
         By add_btn = By.id(app_package_name + "addContactButton");
         driver().findElement(add_btn).click();
-        // The result of clicking doesn't checked.
+
+        //3.Assert title of form
+        assertEquals(driver().findElement(By.id("android:id/title")).getText(), "Add Contact");
         System.out.println("Simplest Appium test done");
     }
 }
