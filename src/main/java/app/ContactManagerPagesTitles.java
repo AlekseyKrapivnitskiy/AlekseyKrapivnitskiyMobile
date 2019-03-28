649ca4da57f9cba8a2f30ff73cc9a99db12e5c5e
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-28 18:08:05 +0300

    fix typo

---
+++ src/main/java/app/ContactManagerPagesTitles.java
@@ -0,0 +1,13 @@
+package app;
+
+public enum ContactManagerPagesTitles {
+
+    START_PAGE_TITLE("Contact Manager"),
+    ADD_CONTACT_PAGE_TITLE("Add Contact");
+
+    public String title;
+
+    ContactManagerPagesTitles(String title) {
+        this.title = title;
+    }
+}
--- src/test/java/scenarios/nativetests/SimpleNativeTests.java
+++ src/test/java/scenarios/nativetests/SimpleNativeTests.java
@@ -6,6 +6,8 @@ import scenarios.Hooks;
 
 import java.io.IOException;
 
+import static app.ContactManagerPagesTitles.ADD_CONTACT_PAGE_TITLE;
+import static app.ContactManagerPagesTitles.START_PAGE_TITLE;
 import static org.testng.Assert.assertEquals;
 
 @Test(groups = "native")
@@ -17,7 +19,7 @@ public class SimpleNativeTests extends Hooks {
     @Test(description = "Just click on button 'Add contact'")
     public void simplestTest() throws Exception {
         //1.Assert title of start page
-        assertEquals(driver().findElement(By.id("android:id/title")).getText(), "Contact Manager");
+        assertEquals(driver().findElement(By.id("android:id/title")).getText(), START_PAGE_TITLE.title);
 
         //2.Click on 'Add Contact' button
         String app_package_name = "com.example.android.contactmanager:id/";
@@ -25,7 +27,7 @@ public class SimpleNativeTests extends Hooks {
         driver().findElement(add_btn).click();
 
         //3.Assert title of form
-        assertEquals(driver().findElement(By.id("android:id/title")).getText(), "Add Contact");
+        assertEquals(driver().findElement(By.id("android:id/title")).getText(), ADD_CONTACT_PAGE_TITLE.title);
         System.out.println("Simplest Appium test done");
     }
 }
